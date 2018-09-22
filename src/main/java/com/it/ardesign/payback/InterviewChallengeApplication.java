cd8c28db37130c6f3a77c8135723c7694d66fa05
Arkadiusz Rogulski <arkadiusz.rogulski@gmail.com>
2018-09-22 16:03:14 +0200

    init commit

---
+++ src/main/java/com/it/ardesign/payback/InterviewChallengeApplication.java
@@ -0,0 +1,12 @@
+package com.it.ardesign.payback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewChallengeApplication.class, args);
+    }
+}
